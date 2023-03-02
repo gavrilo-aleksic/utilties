2cee0be7103e87d70e9d60da47d3542529b88de7
Gavrilo Aleksic <galeksic@apple.com>
2023-03-02 14:37:43 +0100

    Create chain conditional function

--- conditionals/conditionals.ts
+++ conditionals/conditionals.ts
@@ -1,5 +1,9 @@
 /** Function for checking if first argument is true, or if it is false, perform logical AND on second argument */
-export const trueOrNegativeAnd = (condition: boolean, negativeCondition?: boolean) =>
-  condition || (!condition && negativeCondition);
+export const TrueOrNegativeAnd = (
+  condition: boolean,
+  negativeCondition?: boolean
+) => condition || (!condition && negativeCondition);
 
-  
+export const MultipleOr = (...args: boolean[]) => args.reduce((prevValue, currentValue) => prevValue || currentValue, false) 
+
+export const MultipleAnd = (...args: boolean[]) => args.reduce((prevValue, currentValue) => prevValue && currentValue, true) 
