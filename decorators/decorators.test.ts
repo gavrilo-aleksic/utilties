dd1cb6c9ba5485b58b08b0f5f9d05ebc53462376
Gavrilo Aleksic <galeksic@apple.com>
2023-03-03 16:53:20 +0100

    Add more tests for After middleware

--- decorators/decorators.test.ts
+++ decorators/decorators.test.ts
@@ -11,4 +11,14 @@ describe('[Decorators]', () => {
     const result = testInstance.methodWithAfterCallback();
     expect(result).toBe(3);
   })
+  test('After decorator should execute callbacks and return their return value if they are passed in separate After decorator', () => {
+    const testInstance = new TestClass();
+    const result = testInstance.methodWithMultipleAfterCallback();
+    expect(result).toBe(3);
+  })
+  test('After decorator should return original functions return value if last callback doesnt return anything', () => {
+    const testInstance = new TestClass();
+    const result = testInstance.methodWithAfterCallbackNoReturn();
+    expect(result).toBe(0);
+  })
 })
--- mocks/TestClass.ts
+++ mocks/TestClass.ts
@@ -1,6 +1,7 @@
 import { After, TryCatch,MiddlewareCallbackProps } from "../decorators/middleware";
 
 const callback = ({value}: MiddlewareCallbackProps) => value +1;
+const callbackWithNoReturn = () => {}
 export class TestClass {
   public error: Error | null = null;
   
@@ -8,9 +9,20 @@ export class TestClass {
   methodThatThrows() {
     throw new Error("Throwing Error");
   }
-  
+
   @After(callback, callback, callback)
   methodWithAfterCallback(){
+    return 0;
+  }
+  @After(callback, callback, callbackWithNoReturn)
+  methodWithAfterCallbackNoReturn(){
+    return 0;
+  }
+
+  @After(callback)
+  @After(callback)
+  @After(callback)
+  methodWithMultipleAfterCallback(){
     return 0;
   }
 }
