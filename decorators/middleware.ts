0fe1469c0902f9be128869030ba39cb413f533f2
Gavrilo Aleksic <galeksic@apple.com>
2023-03-03 15:00:14 +0100

    Small moving around of decorators;

--- decorators/middleware.ts
+++ decorators/middleware.ts
@@ -27,3 +27,26 @@ export const After = (callback: (props: MiddlewareCallbackProps) => any) => {
   };
 };
 
+/** Wraps the method in try/catch block. If error is thrown, callback is executed and its returned value is returned as function value */
+export const TryCatch =
+  (errorCallback?: (error: any, ...originalArguments: any[]) => any) =>
+  (
+    classConstructor: any,
+    methodName: string,
+    propertyDescriptior: PropertyDescriptor
+  ) => {
+    const oldMethod = propertyDescriptior.value;
+    propertyDescriptior.value = function (...args: any[]) {
+      try {
+        const functionReturnValue = oldMethod.apply(this, [...args]);
+        return functionReturnValue;
+      } catch (error) {
+        if (errorCallback) {
+          return errorCallback.apply(this, [error, ...args]);
+        } else {
+          throw error;
+        }
+      }
+    };
+    return propertyDescriptior;
+  };
