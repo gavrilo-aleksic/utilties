6431ecb3b0c9c97060c75b7f57e86017a7de6af3
Gavrilo Aleksic <galeksic@apple.com>
2023-03-03 13:35:19 +0100

    Create Arithmetic class

--- math/math.test.ts
+++ math/math.test.ts
@@ -1,8 +1,29 @@
-import { limit } from "./limit"
+import { ar } from "./arithmetics";
+import { limit } from "./limit";
 
-describe('[math]', () => {
-  test('[limit]', () => {
-    const calculatedLimit = limit((x) => Math.pow(1 + 1/x, x));
+describe("[math]", () => {
+  test("[limit]", () => {
+    const calculatedLimit = limit((x) => Math.pow(1 + 1 / x, x));
     expect(calculatedLimit.result).toBeCloseTo(Math.E);
-  })
-})
+  });
+});
+
+describe("[Arithmetics]", () => {
+  test("Should correctly add numbers", () => {
+    const arithmetics = ar();
+    arithmetics.add(2, 3, -1).add(2);
+    expect(arithmetics.getValue()).toBe(6);
+  });
+
+  test("Should correctly multiply numbers", () => {
+    const arithmetics = ar(1);
+    arithmetics.multiply(2).add(3).multiply(5);
+    expect(arithmetics.getValue()).toBe(25);
+  });
+
+  test("Should correctly display steps", () => {
+    const arithmetics = ar();
+    arithmetics.subtract(5).subtract(1,2,3);
+    expect(arithmetics.getSteps()).toBe(25);
+  });
+});
