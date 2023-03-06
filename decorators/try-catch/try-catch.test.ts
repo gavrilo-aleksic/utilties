d9a276bafeb1f89d87edb3829c096ab2f7e9ce28
Gavrilo Aleksic <galeksic@apple.com>
2023-03-06 09:36:02 +0100

    Add return type for TryCatch test class for more clarification

--- decorators/try-catch/try-catch.test.ts
+++ decorators/try-catch/try-catch.test.ts
@@ -1,7 +1,8 @@
 import { TryCatchClass } from "./mocks/TryCatchClass";
 
-test("TryCatch decorator should catch error", () => {
+test("TryCatch decorator should catch error and return callback's value", () => {
   const testInstance = new TryCatchClass();
   const result = testInstance.methodThatThrows();
   expect(result).toBeInstanceOf(Error);
+  expect(result?.message).toBe("Caught Error");
 });
