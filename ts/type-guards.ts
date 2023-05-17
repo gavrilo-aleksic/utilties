3a1e73315c31a3610fd9deca425b86f75b303a51
Gavrilo Aleksic <galeksic@apple.com>
2023-05-17 13:47:36 +0200

    small fix

--- ts/type-guards.ts
+++ ts/type-guards.ts
@@ -1,3 +1,3 @@
-export function exists<T>(value: T | null | undefined): value is T {
-  return value !== undefined && value !== null;
-}
+export const exists: <T>(value: T | null | undefined) => value is T = <T>(
+  value: T | undefined | null
+): value is T => value !== undefined && value !== null;
