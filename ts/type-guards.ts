a966aaab6797199215877ef3587a2665f3b1a085
Gavrilo Aleksic <galeksic@apple.com>
2023-05-17 14:16:35 +0200

    Add implicit check-cast to variable based on array of accepted values

--- ts/type-guards.ts
+++ ts/type-guards.ts
@@ -1,2 +1,25 @@
 export const exists = <T>(value: T | undefined | null): value is T =>
   value !== undefined && value !== null;
+
+/** Defines the type of the array elements
+ * @example
+ *
+ * const array = ["A", "B", "C"] as const;
+ * const elementFromArray: ArrayElementType<typeof array> = "A"; <-- type of elementFromArray will not be 'string' but 'A' | 'B' | 'C'
+ */
+export type ArrayElementType<ArrayType extends Readonly<any>> =
+  ArrayType[number];
+
+/** Checks if provided variable is part of the array type and assigns correct type for it
+ * @example
+ *
+ *   const AllowedValues = ["A", "B", "C", "D"] as const;
+ *   const value: any = "B";
+ *   if (isInAllowedValues(value, AllowedValues)) {
+ *      value; <---- 'value' will have type of 'A' | 'B' | 'C' | 'D'
+ *   }
+ */
+export const isInAllowedValues = <T extends Readonly<any>>(
+  value: any,
+  array: T
+): value is ArrayElementType<T> => array.includes(value);
