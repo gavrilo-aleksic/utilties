3c752754a02ca3aff960e5817ac8f68dfd507803
Gavrilo Aleksic <galeksic@apple.com>
2024-01-16 10:51:08 +0100

    Refactor builder pattern without decorators

--- builder/Builder.ts
+++ builder/Builder.ts
@@ -1,80 +1,53 @@
-import "reflect-metadata";
-
 /**
  * Creates a generic Builder function which works based on Lombak's @Builder annotation.
  * Wraps the class with metadata about registered properties and allows chaining of the setters
  */
 
-interface BuilderMeta {
-  fields: BuilderField[];
-}
 type BuilderWrapper<T> = {
   [key in keyof T]: (value: T[key]) => BuilderWrapper<T>;
 } & { build: () => T };
 
-interface BuilderField {
-  key: string;
-}
-
-const BUILDER_META = Symbol("builder-meta");
 const RESERVED_PROPERTY_NAMES = ["build"];
-
-const createBuilderMeta = (modelConstructor: any) =>
-  (modelConstructor[BUILDER_META] = { fields: [] } as BuilderMeta);
+const getHiddenPropertyName = (property: string) => `__${property}__`;
+const getPropertyNameFromHidden = (hiddenName: string) =>
+  hiddenName.slice(2, -2);
+const isHiddenPropertyName = (propertyName: string) =>
+  propertyName.slice(0, 2) == "__" && propertyName.slice(-2) == "__";
 
 export const Builder = <T>(Model: new () => T) => {
-  // @ts-ignore
-  const ModelMeta: BuilderMeta = Model[BUILDER_META];
+  const hiddenKeys: Symbol[] = [];
 
-  if (!ModelMeta)
-    throw new Error(
-      `Class ${Model.name} is not equipped with @Prop() decorators`
-    );
   const wrapper: BuilderWrapper<T> = new Proxy(
     {
       build: () => {
         const newModel = new Model();
-        ModelMeta.fields.forEach(
-          (field: BuilderField) =>
+        Object.keys(wrapper).forEach((hiddenKey: string) => {
+          if (isHiddenPropertyName(hiddenKey))
             // @ts-ignore
-            (newModel[field.key] = wrapper[`_${field.key}`])
-        );
+            newModel[getPropertyNameFromHidden(hiddenKey)] =
+              // @ts-ignore
+              wrapper[hiddenKey];
+        });
         return newModel;
       },
     } as BuilderWrapper<T>,
     {
-      get(target, property) {
-        //@ts-ignore
-        return target[property];
+      get(target: any, property: any) {
+        if (RESERVED_PROPERTY_NAMES.includes(property.toString()))
+          return target[property];
+        if (isHiddenPropertyName(property)) return target[property];
+        return (value: any) => {
+          target[getHiddenPropertyName(property)] = value;
+          return wrapper;
+        };
       },
     }
   );
-
-  ModelMeta.fields.forEach((field: BuilderField) => {
-    let keyofT = field.key as keyof T;
-    // @ts-ignore
-    wrapper[keyofT] = (value: T[typeof keyofT]) => {
-      // @ts-ignore
-      wrapper[`_${keyofT}`] = value;
-      return wrapper;
-    };
-  });
-
   return wrapper;
 };
 
-const Prop = () => (target: any, propertyKey: string) => {
-  if (!target.constructor[BUILDER_META]) createBuilderMeta(target.constructor);
-  if (RESERVED_PROPERTY_NAMES.includes(propertyKey))
-    throw new Error('Property named "build" is reserved keyword');
-  target.constructor[BUILDER_META].fields.push({ key: propertyKey });
-};
-
 export class SomeClass {
-  @Prop()
   public firstName: string = "";
-
-  @Prop()
   public lastName: string = "";
 }
 
